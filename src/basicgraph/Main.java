d1a54c07bcff38cc72d08d9e9bc2b5b0d74dc5f4
Yune <503770873@qq.com>
2018-11-07 16:14:38 +0800

    add path-finding algorithm

--- src/basicgraph/Main.java
+++ src/basicgraph/Main.java
@@ -8,20 +8,30 @@ package basicgraph;
 public class Main {
     public static void main(String[] args){
 
-        // 测试图的联通分量
-        String filename1 = "E:/algorithm/src//basicgraph/testG1.txt";
-        SparseGraph g1 = new SparseGraph(13, false);
-        ReadGraph readGraph1 = new ReadGraph(g1, filename1);
-        Components component1 = new Components(g1);
-        System.out.println("TestG1.txt, Component Count: " + component1.count());
+        // 测试寻路算法
+        String filename = "E:/algorithm/src//basicgraph/testG22.txt";
+        SparseGraph g = new SparseGraph(7, false);
+        ReadGraph readGraph = new ReadGraph(g, filename);
+        g.show();
         System.out.println();
 
-        // TestG2.txt
-        String filename2 = "E:/algorithm/src//basicgraph/testG22.txt";
-        SparseGraph g2 = new SparseGraph(7, false);
-        ReadGraph readGraph2 = new ReadGraph(g2, filename2);
-        Components component2 = new Components(g2);
-        System.out.println("TestG2.txt, Component Count: " + component2.count());
+        Path path = new Path(g,0);
+        System.out.println("Path from 0 to 6 : ");
+        path.showPath(6);
+//        // 测试图的联通分量
+//        String filename1 = "E:/algorithm/src//basicgraph/testG1.txt";
+//        SparseGraph g1 = new SparseGraph(13, false);
+//        ReadGraph readGraph1 = new ReadGraph(g1, filename1);
+//        Components component1 = new Components(g1);
+//        System.out.println("TestG1.txt, Component Count: " + component1.count());
+//        System.out.println();
+//
+//        // TestG2.txt
+//        String filename2 = "E:/algorithm/src//basicgraph/testG22.txt";
+//        SparseGraph g2 = new SparseGraph(7, false);
+//        ReadGraph readGraph2 = new ReadGraph(g2, filename2);
+//        Components component2 = new Components(g2);
+//        System.out.println("TestG2.txt, Component Count: " + component2.count());
 
         // 使用两种图的存储方式读取testG1.txt文件
 //        String filename = "E:/algorithm/src//basicgraph/testG1.txt";
