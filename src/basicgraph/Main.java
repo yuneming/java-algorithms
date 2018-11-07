036bd568c35a2f69661098e0a90182ba93f2f8eb
Yune <503770873@qq.com>
2018-11-07 18:18:05 +0800

    add the shortest path generated by the breadth-first traversal algorithm

--- src/basicgraph/Main.java
+++ src/basicgraph/Main.java
@@ -8,16 +8,49 @@ package basicgraph;
 public class Main {
     public static void main(String[] args){
 
-        // 测试寻路算法
+        // 测试无权图最短路径算法
         String filename = "E:/algorithm/src//basicgraph/testG22.txt";
         SparseGraph g = new SparseGraph(7, false);
         ReadGraph readGraph = new ReadGraph(g, filename);
         g.show();
+
+        // 比较使用深度优先遍历和广度优先遍历获得路径的不同
+        // 广度优先遍历获得的是无权图的最短路径
+        Path dfs = new Path(g,0);
+        System.out.print("DFS : ");
+        dfs.showPath(6);
+
+        ShortestPath bfs = new ShortestPath(g,0);
+        System.out.print("BFS : ");
+        bfs.showPath(6);
+
         System.out.println();
 
-        Path path = new Path(g,0);
-        System.out.println("Path from 0 to 6 : ");
-        path.showPath(6);
+        filename = "E:/algorithm/src//basicgraph/testG1.txt";
+        SparseGraph g2 = new SparseGraph(13, false);
+        ReadGraph readGraph2 = new ReadGraph(g2, filename);
+        g2.show();
+
+        // 比较使用深度优先遍历和广度优先遍历获得路径的不同
+        // 广度优先遍历获得的是无权图的最短路径
+        Path dfs2 = new Path(g2,0);
+        System.out.print("DFS : ");
+        dfs2.showPath(3);
+
+        ShortestPath bfs2 = new ShortestPath(g,0);
+        System.out.print("BFS : ");
+        bfs.showPath(3);
+
+        // 测试寻路算法
+//        String filename = "E:/algorithm/src//basicgraph/testG22.txt";
+//        SparseGraph g = new SparseGraph(7, false);
+//        ReadGraph readGraph = new ReadGraph(g, filename);
+//        g.show();
+//        System.out.println();
+//
+//        Path path = new Path(g,0);
+//        System.out.println("Path from 0 to 6 : ");
+//        path.showPath(6);
 //        // 测试图的联通分量
 //        String filename1 = "E:/algorithm/src//basicgraph/testG1.txt";
 //        SparseGraph g1 = new SparseGraph(13, false);
