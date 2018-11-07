acfee42519c285002da3909a18f91468ca7c217b
Yune <503770873@qq.com>
2018-11-07 14:30:38 +0800

    add depth-first traversal and connectivity components of the graph

--- src/basicgraph/Main.java
+++ src/basicgraph/Main.java
@@ -7,34 +7,50 @@ package basicgraph;
  */
 public class Main {
     public static void main(String[] args){
-        // 使用两种图的存储方式读取testG1.txt文件
-        String filename = "E:/algorithm/src//basicgraph/testG1.txt";
+
+        // 测试图的联通分量
+        String filename1 = "E:/algorithm/src//basicgraph/testG1.txt";
         SparseGraph g1 = new SparseGraph(13, false);
-        ReadGraph readGraph1 = new ReadGraph(g1, filename);
-        System.out.println("test G1 in Sparse Graph:");
-        g1.show();
-
+        ReadGraph readGraph1 = new ReadGraph(g1, filename1);
+        Components component1 = new Components(g1);
+        System.out.println("TestG1.txt, Component Count: " + component1.count());
         System.out.println();
 
-        DenseGraph g2 = new DenseGraph(13, false);
-        ReadGraph readGraph2 = new ReadGraph(g2 , filename );
-        System.out.println("test G1 in Dense Graph:");
-        g2.show();
+        // TestG2.txt
+        String filename2 = "E:/algorithm/src//basicgraph/testG22.txt";
+        SparseGraph g2 = new SparseGraph(7, false);
+        ReadGraph readGraph2 = new ReadGraph(g2, filename2);
+        Components component2 = new Components(g2);
+        System.out.println("TestG2.txt, Component Count: " + component2.count());
 
-        System.out.println();
-        // 使用两种图的存储方式读取testG2.txt文件
-        filename = "E:/algorithm/src//basicgraph/testG2.txt";
-        SparseGraph g3 = new SparseGraph(6, false);
-        ReadGraph readGraph3 = new ReadGraph(g3, filename);
-        System.out.println("test G2 in Sparse Graph:");
-        g3.show();
-
-        System.out.println();
-
-        DenseGraph g4 = new DenseGraph(6, false);
-        ReadGraph readGraph4 = new ReadGraph(g4, filename);
-        System.out.println("test G2 in Dense Graph:");
-        g4.show();
+        // 使用两种图的存储方式读取testG1.txt文件
+//        String filename = "E:/algorithm/src//basicgraph/testG1.txt";
+//        SparseGraph g1 = new SparseGraph(13, false);
+//        ReadGraph readGraph1 = new ReadGraph(g1, filename);
+//        System.out.println("test G1 in Sparse Graph:");
+//        g1.show();
+//
+//        System.out.println();
+//
+//        DenseGraph g2 = new DenseGraph(13, false);
+//        ReadGraph readGraph2 = new ReadGraph(g2 , filename );
+//        System.out.println("test G1 in Dense Graph:");
+//        g2.show();
+//
+//        System.out.println();
+//        // 使用两种图的存储方式读取testG2.txt文件
+//        filename = "E:/algorithm/src//basicgraph/testG2.txt";
+//        SparseGraph g3 = new SparseGraph(6, false);
+//        ReadGraph readGraph3 = new ReadGraph(g3, filename);
+//        System.out.println("test G2 in Sparse Graph:");
+//        g3.show();
+//
+//        System.out.println();
+//
+//        DenseGraph g4 = new DenseGraph(6, false);
+//        ReadGraph readGraph4 = new ReadGraph(g4, filename);
+//        System.out.println("test G2 in Dense Graph:");
+//        g4.show();
 
 
 
